74e5fcdd50fcbdaac90ac1bb84a33200ef502865
Sahil Tanwar <tanwarsahil775@gmail.com>
2026-06-28 13:56:53 +0530

    Update MyService.java

--- Mockito Testing/basic Mockito/MyService.java
+++ Mockito Testing/basic Mockito/MyService.java
@@ -54,3 +54,25 @@ public class MyService {
     }
 }
 
+
+
+public class MyService {
+
+    private ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+
+        api.connect();
+
+        String data = api.getData();
+
+        api.disconnect();
+
+        return data;
+    }
+}
+
