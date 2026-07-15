3227a63761d83db87f17d95e48a7853617b24bb7
Sahil Tanwwar <sahil@example.com>
2026-07-15 22:53:12 +0530

    Revert Week 4 folder reorganization

---
+++ Deep Skilling/Week 1/Mockito Testing/basic Mockito/MyService.java
@@ -0,0 +1,78 @@
+﻿public class MyService {
+    private ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
+
+
+// Exercise 3 ------------------->
+public class MyService {
+
+    ExternalApi api;
+    public MyService(ExternalApi api){
+        this.api = api;
+    }
+
+    public String fetchUser(int id){
+        return  api.getUser(id);
+    }
+}
+
+
+
+public class MyService {
+
+    ExternalApi api;
+    public MyService(ExternalApi api){
+        this.api = api;
+    }
+
+    public void fetchUser(){
+         api.getUser();
+    }
+}
+
+
+
+
+
+public class MyService {
+
+    ExternalApi api;
+    public MyService(ExternalApi api){
+        this.api = api;
+    }
+
+    public String fetchUser(){
+      return    api.getData();
+    }
+}
+
+
+
+public class MyService {
+
+    private ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+
+        api.connect();
+
+        String data = api.getData();
+
+        api.disconnect();
+
+        return data;
+    }
+}
+
