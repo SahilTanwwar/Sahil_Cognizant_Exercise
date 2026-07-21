112c8508b43197dad1186502b0fe530ccbcee5ca
Sahil Tanwwar <sahil@example.com>
2026-07-21 16:18:16 +0530

    Add Week 5 ReactJS-HOL exercise

---
+++ Deep Skilling/Week 5/ReactJS-HOL/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>welcome to the first session of React</h1>
+    </main>
+  );
+}
+
+export default App;
